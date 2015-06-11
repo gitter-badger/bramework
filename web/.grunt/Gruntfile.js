5784245bef452ee287a69fe87f4901f2d91a6db3
Ivan Barcia <mercauditore@gmail.com>
2015-06-11 16:23:06 +0200

    Añadido plugin 'notify' a grunt

--- web/.grunt/Gruntfile.js
+++ web/.grunt/Gruntfile.js
@@ -1,50 +1,51 @@
 module.exports = function (grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-        less: {
-            production: {
-                options: {
-                    cleancss: true,
-                    compress: true
-                },
-                files: {
-                    '../src/css/style.min.css': '../src/css/less/style.less'
-                }
-            },
-          development: {
-                files: {
-                    '../src/css/style.css': '../src/css/less/style.less'
-                }
-            }
+    less: {
+      production: {
+        options: {
+          cleancss: true,
+          compress: true
         },
-
-        uglify: {
-          scripts: {
-            files: {
-              '../src/js/script.min.js': [ '../src/js/scripts/_onload.js',  '../src/js/scripts/*.js', '../src/js/vendor/*.js', '../src/js/polyfills/*.js']
-            }
-          }
-        },
-
-        watch: {
-            styles: {
-                files: ['../src/css/less/**/*.less'],
-                tasks: ['less']
-            },
-            scripts: {
-              files: ['../src/js/scripts/*.js', '../src/js/vendor/*.js'],
-              tasks: ['uglify']
-            }
+        files: {
+          '../src/css/style.min.css': '../src/css/less/style.less'
         }
-    });
+      },
+      development: {
+        files: {
+          '../src/css/style.css': '../src/css/less/style.less'
+        }
+      }
+    },
 
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+    uglify: {
+      scripts: {
+        files: {
+          '../src/js/script.min.js': [ '../src/js/scripts/_onload.js',  '../src/js/scripts/*.js', '../src/js/vendor/*.js', '../src/js/polyfills/*.js']
+        }
+      }
+    },
 
-    grunt.registerTask('default', ['watch']);
-    grunt.registerTask('all', ['less', 'uglify']);
-    grunt.registerTask('css', ['less']);
-    grunt.registerTask('js', ['uglify']);
+    watch: {
+      styles: {
+        files: ['../src/css/less/**/*.less'],
+        tasks: ['less']
+      },
+      scripts: {
+        files: ['../src/js/scripts/*.js', '../src/js/vendor/*.js', '../src/js/polyfills/*.js'],
+        tasks: ['uglify']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-notify');
+
+  grunt.registerTask('default', ['watch']);
+  grunt.registerTask('all', ['less', 'uglify']);
+  grunt.registerTask('css', ['less']);
+  grunt.registerTask('js', ['uglify']);
 };
