dddf5f25fdfbbcce3cd41c39ff9d758a3fd4e815
Ivan Barcia <mercauditore@gmail.com>
2015-06-25 16:19:42 +0200

    cambio de estructura y mejoras en el navbar

--- web/src/js/scripts/navbar.js
+++ web/src/js/scripts/navbar.js
@@ -2,9 +2,9 @@ var navbar = document.getElementsByClassName("js-navbar");
 
 function navbarScroll() {
   if (window.scrollY < 1) {
-    navbar[0].classList.remove("navbar-scrolled")
+    navbar[0].classList.remove("navbar_scrolled")
   }
   else {
-    navbar[0].classList.add("navbar-scrolled")
+    navbar[0].classList.add("navbar_scrolled")
   }
 }
