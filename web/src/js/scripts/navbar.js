7853b373e36b3b2be1d861739b5d469fe6fe2318
Ivan Barcia <mercauditore@gmail.com>
2015-07-07 18:21:58 +0200

    Mejoras en el Navbar: navba con fondo de imagen

--- web/src/js/scripts/navbar.js
+++ web/src/js/scripts/navbar.js
@@ -1,10 +1,13 @@
 var navbar = document.getElementsByClassName("js-navbar");
+var wrapper = document.getElementsByClassName("js-wrapper");
 
 function navbarScroll() {
   if (window.scrollY < 1) {
-    navbar[0].classList.remove("navbar_scrolled")
+    navbar[0].classList.remove("navbar-scrolled")
+    wrapper[0].classList.remove("wrapper-scrolled")
   }
   else {
-    navbar[0].classList.add("navbar_scrolled")
+    navbar[0].classList.add("navbar-scrolled")
+    wrapper[0].classList.add("wrapper-scrolled")
   }
 }
