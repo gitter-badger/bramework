aea169c9f10822ea77ef7b832b359cd33f347685
Iván Barcia <mercauditore@gmail.com>
2015-10-26 20:08:31 +0100

    Correción sombras md-navbar

--- web/src/js/scripts/navbar.js
+++ web/src/js/scripts/navbar.js
@@ -2,9 +2,9 @@ var navbar = document.getElementsByClassName("js-navbar");
 
 function navbarScroll() {
   if (window.scrollY < 5) {
-    navbar[0].classList.remove("navbar-scrolled")
+    navbar[0].classList.remove("md-navbar-scrolled")
   }
   else {
-    navbar[0].classList.add("navbar-scrolled")
+    navbar[0].classList.add("md-navbar-scrolled")
   }
 }
